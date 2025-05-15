4d381b0abd820aa364aeab8f25ad4090d851095d
Muhammed Midlaj PC <95988724+muhammedmidlajpc@users.noreply.github.com>
2025-05-15 16:15:47 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,13 @@ const user = require("./routes/user.routes");
 const blog = require("./routes/blog.routes");
 const app = express();
 
-app.use(cors({origin: "https://blog-front-end-0bz2.onrender.com",credentials: true}));
+app.use(
+  cors({
+    origin: "https://blog-front-end-0bz2.onrender.com",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  })
+);
 app.use(express.json());
 app.use(user);
 app.use(blog);
